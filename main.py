77945d42fd6464402419ab7dfa5363ce852cabd0
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-03-26 05:47:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -58,5 +58,46 @@ add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
 
 
-#-----------------------
-#silent action
+#-----------------------silent action
+bids={}
+
+running = True
+while running: #same as: while running == True
+    print('\033c') #Use this to clear screen between users
+    print('----------------SECRET AUCTION-----------------\n')
+    user_name = input("Enter your name:\n")
+    user_bid = int(input("Your bid:\n$"))
+
+    #add each element into the new dict
+    bids[user_name]=user_bid
+
+    running_flag = input("Another bid?\n(enter 'y' for yes, or 'n' for no)\n")
+    if running_flag == 'n':
+        running = False
+        print('\033c') #clear screen
+    #else - don't need it here, because it'll pass this way anyways
+
+#find the key that has highest value
+max_key=max(bids, key=bids.get)
+#find the highest value
+max_value=max(bids.values())
+
+'''
+#or we can go simple way, without fancy functions:
+#var to keep the heighest bidder
+highest_bid = 0
+#var to keep winners name
+winner = ''
+#loop through
+for bidder in bids:
+#assign to var
+bid_amount = bids[bidder]
+#if it's bigger, change it
+if bid_amount > highest_bid: 
+    highest_bid = bid_amount
+    winner = bidder
+print(f"The winner is {winner} with a bid of ${highest_bid}")
+'''
+
+print('----------------SECRET AUCTION-----------------\n')
+print(f'\nThe winner is _{max_key}_ with the highest bid of _${max_value}_\n')
